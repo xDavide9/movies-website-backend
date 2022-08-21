349134159e7d7c326f927e6cdbb341ab42adf7b3
Dav <biancodavide646@gmail.com>
2022-08-21 15:34:40 +0200

    add requests based on most popular, time and genre

--- src/routes/discover.js
+++ src/routes/discover.js
@@ -2,14 +2,63 @@ const express = require("express");
 const axios = require("axios");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  // get the most popular films in descending order from the api
+router.get("/popular", (req, res) => {
+  // get the most popular films of all times
   axios
     .get(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&sort_by=popularity.desc`
     )
     .then((response) => {
-      res.send(response.data);
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get("/popular/range", (req, res) => {
+  // get the most popular films from a specific interval of time
+  // yyyy-mm-dd
+  axios
+    .get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&primary_release_date.gte=${req.query.from}&primary_release_date.lte=${req.query.to}&sort_by=popularity.desc`
+    )
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get("/genre", (req, res) => {
+  // get the most popular film of each genre that is associated with an id
+  // 28 action
+  // 12 adventure
+  // 16 animation
+  // 35 comedy
+  // 80 crime
+  // 99 documentary
+  // 18 drama
+  // 10751 family
+  // 14 fantasy
+  // 36 history
+  // 27 horror
+  // 10402 music
+  // 9648 mystery
+  // 10749 romance
+  // 878 science fiction
+  // 10770 tv movie
+  // 53 thriller
+  // 10752 war
+  // 37 western
+
+  axios
+    .get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=${req.query.id}&sort_by=popularity.desc`
+    )
+    .then((response) => {
+      res.json(response.data);
     })
     .catch((error) => {
       console.log(error);
